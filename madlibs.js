2dcafc3eed2fb1aaf0f63d19205128b03fef7f2e
Zryan Muhammed Jaafer <zryan.muhammed.jaafer@gmail.com>
2022-08-24 00:48:38 +0300

    3.0v

--- madlibs.js
+++ madlibs.js
@@ -26,41 +26,85 @@
  * There are multiple ways to do this, but you may want to use regular expressions.
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
+const POS={
+  n:"noun",
+  v:"verb",
+  a:"adj"
+}
+function findPOS(pos){
+  const fixedPOS=pos.replace("[","").replace("]","");
+  return POS[fixedPOS];
+}
+const OUR_REGEX=/(?<word>\w+)(?<pos>\[[nva]\])?(?<punc>[\.,])?/;
 function parseStory(rawStory) {
     const arrStory=rawStory.split(" ");
     const fixedArray=[];
-    for(let e of arrStory){
-      if(/[[]a]/.test(e) && /[.,]/.test(e)){
-      fixedArray.push({word:e.replace("[a]","").substring(0,e.length-1),pos:"adj"});
-      fixedArray.push({word:e.substring(e.length-1)})
+    for(let el of arrStory){
+      const group=OUR_REGEX.exec(el).groups;
+       if(group.pos!=undefined && group.punc==undefined){
+        fixedArray.push({
+          word:group.word,
+          pos:findPOS(group.pos)
+        });
       }
-      else if(/[[]a]/.test(e) && !(/[.,]/.test(e))){
-        fixedArray.push({word:e.replace("[a]",""),pos:"adj"});
+      else if(group.pos==undefined && group.punc!=undefined){
+        fixedArray.push({
+          word:group.word
+        });
+        fixedArray.push({
+          word:group.punc
+        });
       }
-      else if(/[[]v]/.test(e) && /[.,]/.test(e)){
-        fixedArray.push({word:e.replace("[v]","").substring(0,e.length-1),pos:"verb"});
-        fixedArray.push({word:e.substring(e.length-1)})
-        }
-      else if(/[[]v]/.test(e) && !/[.,]/.test(e)){
-        fixedArray.push({word:e.replace("[v]",""),pos:"verb"});
+      else if(group.pos!=undefined && group.punc!=undefined){
+        fixedArray.push({
+          word:group.word,
+          pos:findPOS(group.pos)
+        });
+        fixedArray.push({
+          word:group.punc
+        });
       }
-      else if(/[[]n]/.test(e) && /[.,]/.test(e)){
-        fixedArray.push({word:e.replace("[n]","").substring(0,e.length-1),pos:"noun"});
-        fixedArray.push({word:e.substring(e.length-1)})
-        }
-      else if(/[[]n]/.test(e) && !/[.,]/.test(e)){
-        fixedArray.push({word:e.replace("[n]",""),pos:"noun"});
-      }
-      else if(/[.,]/.test(e)){
-        fixedArray.push({word:e.replace("\n","").substring(0,e.length-1)});
-        fixedArray.push({word:e.substring(e.length-1)})
-      }
-      else{
-        fixedArray.push({word:e.replace("\n","")});
-      }
+     else{
+      fixedArray.push({
+      word:group.word
+      });
+     }
     }
-    return fixedArray;
-}
+    return fixedArray; 
+  }
+     
+  
+
+    // for(let e of arrStory){
+    //   if(/([[]a])/.test(e) && /[.,]/.test(e)){
+    //   fixedArray.push({word:e.replace("[a]","").substring(0,e.length-1),pos:"adj"});
+    //   fixedArray.push({word:e.substring(e.length-1)})
+    //   }
+    //   else if(/[[]a]/.test(e) && !(/[.,]/.test(e))){
+    //     fixedArray.push({word:e.replace("[a]",""),pos:"adj"});
+    //   }
+    //   else if(/[[]v]/.test(e) && /[.,]/.test(e)){
+    //     fixedArray.push({word:e.replace("[v]","").substring(0,e.length-1),pos:"verb"});
+    //     fixedArray.push({word:e.substring(e.length-1)})
+    //     }
+    //   else if(/[[]v]/.test(e) && !/[.,]/.test(e)){
+    //     fixedArray.push({word:e.replace("[v]",""),pos:"verb"});
+    //   }
+    //   else if(/[[]n]/.test(e) && /[.,]/.test(e)){
+    //     fixedArray.push({word:e.replace("[n]","").substring(0,e.length-1),pos:"noun"});
+    //     fixedArray.push({word:e.substring(e.length-1)})
+    //     }
+    //   else if(/[[]n]/.test(e) && !/[.,]/.test(e)){
+    //     fixedArray.push({word:e.replace("[n]",""),pos:"noun"});
+    //   }
+    //   else if(/[.,]/.test(e)){
+    //     fixedArray.push({word:e.replace("\n","").substring(0,e.length-1)});
+    //     fixedArray.push({word:e.substring(e.length-1)})
+    //   }
+    //   else{
+    //     fixedArray.push({word:e.replace("\n","")});
+    //   }
+    
 
 
 
@@ -80,14 +124,15 @@ function parseStory(rawStory) {
 getRawStory()
   .then(parseStory)
   .then((processedStory) => {
+
     console.log(processedStory);
+
   const madLibsEdit=document.querySelector(".madLibsEdit");
   const madLibsPreview=document.querySelector(".madLibsPreview");
   const pEdit=document.createElement('p');
   const pPrev=document.createElement('p');
   madLibsEdit.append(pEdit);
   madLibsPreview.append(pPrev);
- 
   for(let wp of processedStory){
     if(wp.pos!=undefined){
     const input=document.createElement('input');
@@ -98,16 +143,18 @@ getRawStory()
     input2.placeholder=wp.pos;
     input.maxLength=20;
     input2.readOnly=true;
+    
     input.addEventListener('input',()=>{
       input2.value=input.value;
     });
-      input.addEventListener('keydown', e => {
-          if(e.keyCode === 13) {
-              let nextEl = input.nextElementSibling;
-              if(nextEl!=null){
+    
+    input.addEventListener('keydown', e => {
+      if(e.keyCode === 13) {
+        let nextEl = input.nextElementSibling;
+            if(nextEl!=null){
               if(nextEl.nodeName === 'INPUT') {
-                  nextEl.focus();
-              }else {
+              nextEl.focus();
+               }else {
                   alert('done');
               }
           }
@@ -124,7 +171,6 @@ getRawStory()
       pPrev.append(` ${wp.word}`);
       }
     }
-   }
-  
-    
-});
+  }
+   
+ });
